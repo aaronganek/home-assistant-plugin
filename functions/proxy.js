2fc9ab341504870037eb3ec7ee10d04124c81d9e
aaronganek <aaron.ganek@gmail.com>
2023-04-09 13:40:24 -0700

    Adding print statements to proxy.js

--- functions/proxy.js
+++ functions/proxy.js
@@ -1,20 +1,21 @@
 const axios = require('axios');
 
 exports.handler = async function (event, context) {
+  console.log('Received event:', JSON.stringify(event)); // Log the received event
   const targetUrl = 'https://wdiiuv7qa8yxuax5l7z513u7vtj53orr.ui.nabu.casa' + event.path;
   const method = event.httpMethod;
-  const data = event.body ? JSON.parse(event.body) : null; // Check for empty event.body before parsing
+  const data = event.body ? JSON.parse(event.body) : null;
 
-  // Construct the Authorization header for Basic Authentication
   const username = 'chatgpt';
   const password = 'lahs9s8d231!';
   const authHeaderValue = 'Basic ' + Buffer.from(username + ':' + password).toString('base64');
 
-  // Create a new headers object with only the headers you want to forward
   const headers = {
     'Content-Type': 'application/json',
     'Authorization': authHeaderValue,
   };
+
+  console.log('Sending request to target URL:', targetUrl); // Log the target URL
 
   try {
     const response = await axios({
@@ -23,11 +24,13 @@ exports.handler = async function (event, context) {
       headers: headers,
       data: data,
     });
+    console.log('Received response:', JSON.stringify(response.data)); // Log the received response
     return {
       statusCode: response.status,
       body: JSON.stringify(response.data),
     };
   } catch (error) {
+    console.error('Error occurred:', JSON.stringify(error.response.data)); // Log the error
     return {
       statusCode: error.response.status,
       body: JSON.stringify(error.response.data),
